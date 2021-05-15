351f19e2cb92207947440a805dd48854cf447b15
unknown <wkdus7h@naver.com>
2021-05-16 00:26:35 +0900

    create token and verfiying it

--- blog-sample/blog-backend/src/api/auth/auth.ctrl.js
+++ blog-sample/blog-backend/src/api/auth/auth.ctrl.js
@@ -25,7 +25,7 @@ export const register = async ctx => {
   try {
     const exists = await User.findByUsername(username);
     if (exists) {
-      ctx.status = 409; // conflict
+      ctx.status = 409; // Conflict
       return;
     }
 
@@ -35,11 +35,24 @@ export const register = async ctx => {
     await user.setPassword(password);
     await user.save();
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e); // Internel Server error
   }
 };
 
+/*	
+  POST /api/auth/login	
+  {	
+    username: 'frantz',	
+    password: 'mypass123'	
+  }	
+*/
 export const login = async ctx => {
   const { username, password } = ctx.request.body;
 
@@ -62,11 +75,34 @@ export const login = async ctx => {
       return;
     }
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e); // Internel Server error
   }
 };
 
-export const check = async ctx => {};
+/*	
+  GET /api/auth/check	
+*/
+export const check = async ctx => {
+  const { user } = ctx.state;
+  // 로그인 중이 아님
+  if (!user) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
+};
 
-export const logout = async ctx => {};
+/*	
+  POST /api/auth/logout	
+*/
+export const logout = async ctx => {
+  ctx.cookies.set('access_token');
+  ctx.status = 204; // No Content
+};
