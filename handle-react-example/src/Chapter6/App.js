ee930f0ca77e7dd39b7f0991f3016c7d973d3dbb
unknown <wkdus7h@naver.com>
2021-05-27 12:51:37 +0900

    add app.js

---
+++ handle-react-example/src/Chapter6/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import IterationSample from './Chapter6/IterationSample';
+
+class App extends Component {
+  render() {
+    return <IterationSample />;
+  }
+}
+
+export default App;
