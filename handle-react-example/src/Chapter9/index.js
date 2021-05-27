ce1931e384c3a1bb09d36987b58a5574ea2fdc91
unknown <wkdus7h@naver.com>
2021-05-27 19:13:47 +0900

    add scss and before eject

--- handle-react-example/src/Chapter9/index.js
+++ handle-react-example/src/Chapter9/index.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SassComponent from './SassComponent';
 
 class Index extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <img src={logo} alt="logo" className="logo" />
-          <p>
-            Edit <code>src/App.js</code> ans save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SassComponent />
       </div>
     );
   }
