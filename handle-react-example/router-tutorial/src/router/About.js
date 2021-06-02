a7fb547a8a30ae2744234d7681e2c256f769a1b2
unknown <wkdus7h@naver.com>
2021-06-02 18:45:35 +0900

    Add qs library and URL query

--- handle-react-example/router-tutorial/src/router/About.js
+++ handle-react-example/router-tutorial/src/router/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 쿼리문 맨앞의 ? 생략
+  });
+
+  const showDetail = query.detail === 'true';
+
   return (
     <div>
       <h1>소개</h1>
       <p>소개 페이지!</p>
+      <h3>{query.detail}</h3>
     </div>
   );
 };
