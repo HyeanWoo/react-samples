1a925e357a9ff9c66a2ebf38d4bc9a3edd7fd9a4
unknown <wkdus7h@naver.com>
2021-06-03 11:00:51 +0900

    Add NavLink for style

--- handle-react-example/router-tutorial/src/router/Profiles.js
+++ handle-react-example/router-tutorial/src/router/Profiles.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+  const activeStyle = {
+    background: 'black',
+    color: 'white',
+  };
+
   return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/spaceboy">spaceboy 프로필</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/spaceboy">
+            spaceboy 프로필
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/hunter">hunter 프로필</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/hunter">
+            hunter 프로필
+          </NavLink>
         </li>
       </ul>
 
