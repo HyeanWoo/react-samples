61eafc39c1bc69f24658572207cd23d8776179fe
unknown <wkdus7h@naver.com>
2021-06-03 10:59:10 +0900

    Add switch

--- handle-react-example/router-tutorial/src/App.js
+++ handle-react-example/router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './router/About';
 import HistorySample from './router/HistorySample';
 import Home from './router/Home';
@@ -23,10 +23,20 @@ const App = () => {
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact />
-      <Route path={['/about', '/info']} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>존재하지 않는 페이지입니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
