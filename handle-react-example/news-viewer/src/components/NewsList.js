a01847951d7779abe0bc7f11bf93854cf731423d
unknown <wkdus7h@naver.com>
2021-06-03 17:22:38 +0900

    Link to news api

--- handle-react-example/news-viewer/src/components/NewsList.js
+++ handle-react-example/news-viewer/src/components/NewsList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+// import axios from 'axios';
 import NewsItem from './NewsItem';
+import { getAllArticles } from '../lib/articles';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
-  padding-top: 3rem;
+  padding-bottom: 3rem;
   width: 768px;
   margin: 0 auto;
   margin-top: 2rem;
@@ -15,22 +17,57 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [isMaximumRequest, setIsMaximumRequest] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      // try {
+      //   const response = await axios.get(
+      //     'https://newsapi.org/v2/top-headlines?country=kr&apiKey=2a0f950e51a54f5c86c981a24e402e71',
+      //   );
+      //   setArticles(response.data.articles);
+      // } catch (e) {
+      //   console.log(e.message);
+      //   const errorCode = e.message.replace(/^\D+/g, '');
+      //   console.log(Number(errorCode) === 429);
+      //   if (Number(errorCode) === 429) {
+      //     setIsMaximumRequest(true);
+      //   }
+      // }
+      try {
+        const response = await getAllArticles();
+        setArticles(response.articles);
+      } catch (e) {
+        console.log(e);
+        setIsMaximumRequest(true);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>불러오는 중...</NewsListBlock>;
+  }
+
+  if (isMaximumRequest) {
+    return <NewsListBlock>API 요청량 초과</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
---
+++ handle-react-example/news-viewer/src/lib/articles.js
@@ -0,0 +1,330 @@
+const data = {
+  status: 'ok',
+  totalResults: 34,
+  articles: [
+    {
+      source: {
+        id: 'google-news',
+        name: 'Google News',
+      },
+      author: null,
+      title:
+        "英, 이달 말 봉쇄 해제 '먹구름'...'인도 변이' 초비상 / YTN - YTN news",
+      description: null,
+      url: 'https://news.google.com/__i/rss/rd/articles/CBMiK2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9bWZMaFk1ZDd2Z3PSAQA?oc=5',
+      urlToImage: null,
+      publishedAt: '2021-06-03T06:22:54Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Joins.com',
+      },
+      author: '이민정',
+      title:
+        '백신 효과 자신 없나…접종률 40%에도 봉쇄 못푸는 中 - 중앙일보 - 중앙일보',
+      description:
+        '2일(현지시간) 블룸버그통신은 중국이 높은 백신 접종률에도 국경 폐쇄를 유지하는 등 방역 고삐를 풀지 않고 있다고 현지 분위기를 전했다. 실제 시노팜 백신에 의존하고 있는 인도양 섬나라 세이셸도 5월 초까지 세계에서 가장 높은 백신 접종률을 보였지만 최근 접종 완료자의 감염 사례가 37%에 육박해 중국산 백신 효 - 백신,접종률,백신 접종률,백신 부작용,백신 현금,코로나19,covid19',
+      url: 'https://news.joins.com/article/24073668',
+      urlToImage:
+        'https://pds.joins.com/news/component/htmlphoto_mmdata/202106/03/19ab6cf7-2431-4288-9b2d-68bc17e13ae3.jpg',
+      publishedAt: '2021-06-03T06:12:13Z',
+      content: '',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Sedaily.com',
+      },
+      author: '박신원',
+      title:
+        "국회의원 132명 '도쿄올림픽 지도 독도 삭제' 규탄결의안 발의 - 서울경제 - 서울경제신문",
+      description:
+        '더불어민주당 백혜련, 박정, 서영교 의원 등이 3일 서울 여의도 국회 소통관에서 도쿄올림픽조직위원회의 일본 영토 지도 내 독도 표기를 규탄하는 결의안에 대해 기자회견을 하고 있다. /연합뉴스국회의원 132명은 ..',
+      url: 'https://www.sedaily.com/NewsVIew/22NHSIAS86',
+      urlToImage: 'https://newsimg.sedaily.com/2021/06/03/22NHSIAS86_1.jpg',
+      publishedAt: '2021-06-03T06:04:30Z',
+      content:
+        'viewer\r\n , , 3 . /\r\n132 3 . 128, 3, 1 .\r\n " , " . " " " " .\r\nviewer\r\n (), . /= \r\n(IOC) "2018 " . " " .\r\nMBC " " "IOC ( ) . " . " " " " .\r\n/ shin01@sedaily.com\r\n , .',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Ytn.co.kr',
+      },
+      author: '김혜린',
+      title:
+        '[단독] 호텔서 마약 투약 뒤 \'셀프 신고\'..."환각 증세에 무서워서" - YTN',
+      description:
+        "[앵커]어떤 여성의 다급한 신고를 받고 경찰이 서울 강남의 한 호텔에 출동했습니다.현장에 가보니 마약을 맞은 여성이 환각 증세에 빠져 이른바 '셀프 신고'를...",
+      url: 'https://www.ytn.co.kr/_ln/0103_202106031457598222',
+      urlToImage:
+        'https://image.ytn.co.kr/general/jpg/2021/0603/202106031457598222_t.jpg',
+      publishedAt: '2021-06-03T05:57:00Z',
+      content: null,
+    },
+    {
+      source: {
+        id: 'google-news',
+        name: 'Google News',
+      },
+      author: null,
+      title:
+        '1차접종 38만1천551명↑ 접종률 13.1%…60∼74세 예약률 77.6%(종합) - 매일경제',
+      description: null,
+      url: 'https://news.google.com/__i/rss/rd/articles/CBMiNmh0dHBzOi8vd3d3Lm1rLmNvLmtyL25ld3Mvc29jaWV0eS92aWV3LzIwMjEvMDYvNTM3MDMwL9IBOGh0dHBzOi8vbS5tay5jby5rci9uZXdzL3NvY2lldHkvdmlldy1hbXAvMjAyMS8wNi81MzcwMzAv?oc=5',
+      urlToImage: null,
+      publishedAt: '2021-06-03T05:31:05Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Hani.co.kr',
+      },
+      author: null,
+      title:
+        '[전국지표조사] 이준석 36%, 후보 4명 합산 19%…여론조사 독주 - 한겨레',
+      description: 'TK 38%, 국민의힘 지지층 53%',
+      url: 'https://www.hani.co.kr/arti/politics/assembly/997872.html',
+      urlToImage:
+        'http://flexible.img.hani.co.kr/flexible/normal/640/418/imgdb/original/2021/0602/20210602500713.jpg',
+      publishedAt: '2021-06-03T05:03:08Z',
+      content: '·\r\n[] 36%, 4 19% \r\n :2021-06-03 14:03 :2021-06-03 14:05',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Gamemeca.com',
+      },
+      author: '게임메카 류종화 기자',
+      title: '우주와 은하가 담긴 듀얼센스 컨트롤러 2종, 10일 정발 - 게임메카',
+      description:
+        '소니인터랙티브엔터테인먼트코리아(SIEK)는 듀얼센스 무선 컨트롤러 신규 색상인 미드나이트 블랙과 코스믹 레드를 오는 6월 10일 출시한다고 밝혔다. 이번에 처음으로 추가된 듀얼센스 신규 색상은 우주와 은하에서 영감을 얻은 색 배치를 띄고 있다',
+      url: 'https://www.gamemeca.com/view.php?gid=1661062',
+      urlToImage:
+        'https://cdn.gamemeca.com/gmdata/0001/661/062/gm802218_125685683_623115.jpg',
+      publishedAt: '2021-06-03T05:01:33Z',
+      content:
+        "<ul><li> 90% , '' </li><li> , </li><li> MSI, </li><li>: , ' ' </li><li>--, 30 </li><li> : , </li><li> : , </li><li> , 1 </li><li> , 6 10 7 </li><li>[] 3</li></ul><ul><li> , </li><li> 90% , '' </li><l… [+130 chars]",
+    },
+    {
+      source: {
+        id: 'google-news',
+        name: 'Google News',
+      },
+      author: null,
+      title:
+        '[KBS NEWS D-Live] 도쿄올림픽 D-50 "독도 표기 일본 규탄" /공군 부사관 성추행 가해자 구속 /전국 비 "강풍 주의" /6월 3일 14:00~16:00 - KBS News',
+      description: null,
+      url: 'https://news.google.com/__i/rss/rd/articles/CBMiK2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9eEZhQjVIZGxXb3PSAQA?oc=5',
+      urlToImage: null,
+      publishedAt: '2021-06-03T04:55:24Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Ytn.co.kr',
+      },
+      author: '강진원',
+      title: "'택시기사 폭행' 이용구 차관을 둘러싼 '4갈래 수사' - YTN",
+      description:
+        '[앵커]택시기사 폭행 혐의 등으로 검찰과 경찰의 수사를 받는 이용구 법무부 차관.논란 끝에 지난달 28일 사의를 표명했습니다.취임 6개월 만인데, 변호사 시절...',
+      url: 'https://www.ytn.co.kr/_ln/0103_202106031352088276',
+      urlToImage:
+        'https://image.ytn.co.kr/general/jpg/2021/0603/202106031352088276_t.jpg',
+      publishedAt: '2021-06-03T04:52:00Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Inews24.com',
+      },
+      author: '',
+      title: "미호요, '원신' 오는 9일 에픽게임즈 스토어에 출시 - 아이뉴스24",
+      description: '을',
+      url: 'http://www.inews24.com/view/1372834',
+      urlToImage:
+        'http://img.lb.inews24.com/facebookexternalhit/face/626x352/image_gisa/202106/162269493597_1_133631.jpg',
+      publishedAt: '2021-06-03T04:36:00Z',
+      content:
+        "[24 ] '' 9 3 .\r\nPC .\r\n 3 6 9 .\r\n (Liu Wei) \" \" \"PC \" .\r\n'' (RPG). , PC, , iOS 6 9 1.6 '! ? !' .\r\n/ (mj@inews24.com)",
+    },
+    {
+      source: {
+        id: null,
+        name: 'Donga.com',
+      },
+      author: null,
+      title:
+        '“젼젼 남편하고 싶어요”…‘전지현 남편’ 최준혁의 ‘카톡 프사’ - 동아일보',
+      description:
+        '“젼젼 남편하고 싶어요”…‘전지현 남편’ 최준혁 ‘카톡 프사’  알파자산운용 최준혁 대표(40)가 메신저 카카오톡(카톡) 프로필 사진으로 아내인 배우 전지현 씨(40)와의 이혼설…',
+      url: 'https://www.donga.com/news/Economy/article/all/20210603/107251432/2',
+      urlToImage:
+        'https://dimg.donga.com/a/597/0/90/5/wps/NEWS/IMAGE/2021/06/03/107251448.2.jpg',
+      publishedAt: '2021-06-03T04:14:00Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Mk.co.kr',
+      },
+      author: null,
+      title: '정부 "대구시 주선 화이자 백신 진위 의심…구매 안한다" - 매일경제',
+      description:
+        '정부는 대구시가 주선한 화이자사(社)의 신종 코로나바이러스 감염증(코로나19) 백신의 진위가 의심된다며 구매를 추진하지 않기로 했다.  손영래 중앙사고수습본부(중수본) 사회전략반장은 3일 백브리핑에서 대구시의 화이자 백신 구매 주선과 관련해 "백신의 신뢰성에 문제',
+      url: 'https://www.mk.co.kr/news/society/view/2021/06/536342/',
+      urlToImage:
+        'https://file.mk.co.kr/meet/yonhap/2021/06/03/image_readtop_2021_536342_0_120612.jpg',
+      publishedAt: '2021-06-03T03:05:09Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Chosun.com',
+      },
+      author: null,
+      title:
+        '대법 “네이버, 통유리 사옥 반사광 피해 주민에 배상하라” - 조선일보 - 조선일보',
+      description:
+        'IT업체 네이버의 통유리 사옥이 이웃 주민들에게 미치는 태양 반사광 피해에 대해 대법원이 주민들의 손을 들어줬다.',
+      url: 'https://www.chosun.com/national/court_law/2021/06/03/ZC7NKJHKXZEOBEHVMENBOYNP5Q/',
+      urlToImage:
+        'https://images.chosun.com/resizer/dev8ZgfWjpp0BAo1IymN5OJaWPs=/640x336/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/6B3RPQP4RFX4QI657N4CLNP3SQ.jpg',
+      publishedAt: '2021-06-03T02:51:00Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Segye.com',
+      },
+      author: 'lee_owl@segye.com 이승구',
+      title: "여름 필수템 '자외선 차단제'…올바른 사용법은? - 세계일보",
+      description:
+        '최근 낮 기온이 20도 중후반대를 오르내리고, 한낮에 강한 햇빛이 내리쬐는 초여름 날씨가 이어지고 있다. 이럴 땐 점점 강해지는 유해 자외선으로부터 피부를 지켜야 한다.  외출할 때 선크림으로 불리는 자외선 차단제를 꼭 바르고 나가야 할 시기다. 신종 코로나바이러스 감염증(코로나19)으로 인해 마스크는 써야하지만 자외선 차단제를 바르는 것을 잊으면 안된다. 노원을지대학교병원 피부과 이현경 교수는 3일 초여름 날씨에 필수적인 자외선 차단제의 올바른 사용법 등을 …',
+      url: 'https://www.segye.com/newsView/20210603508131',
+      urlToImage:
+        'https://img.segye.com/content/image/2021/06/03/20210603508122.jpg',
+      publishedAt: '2021-06-03T02:50:26Z',
+      content:
+        '2 \r\n . \r\n20 , . . \r\n . (19) .\r\n 3 .\r\n , .  \r\n , . .\r\n . . . \r\n . \r\n (SPF) A (PA) . \r\n B(UVB) . 30 . PA + + . SPF PA . \r\n . . \r\n 6 , . , .\r\n 2 . . \r\n 2mg , 0.5~0.8mg . , , , . \r\n , , 20~30 , 2 .\r\n lee… [+35 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Joins.com',
+      },
+      author: '이지영',
+      title:
+        '문 대통령, ‘공군 부사관 성추행’ 사건에 엄정 수사 지시…“가슴 아파” - 중앙일보 - 중앙일보',
+      description:
+        '문재인 대통령은 3일 성추행 피해 공군 부사관의 사망 사건과 관련해 가해자의 범행에 대해 엄정한 수사를 지시했다. 문 대통령은 "절망스러웠을 피해자를 생각하면 가슴이 아프다"며 "피해 신고 이후 부대 내 처리, 상급자와 동료들의 2차 가해, 피해호소 묵살, 사망 이후 조치 미흡 등에 대해 엄중한 수사와 조치가 있어 - 대통령,성추행,공군 부사관,선임 부사관,이모 공군',
+      url: 'https://news.joins.com/article/24073518',
+      urlToImage:
+        'https://pds.joins.com/news/component/htmlphoto_mmdata/202106/03/73b55261-8041-443e-8357-3aa7ba675eb7.jpg',
+      publishedAt: '2021-06-03T02:39:57Z',
+      content: '',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Gamefocus.co.kr',
+      },
+      author: null,
+      title:
+        "세가퍼블리싱코리아 '소닉 컬러즈 얼티밋', 신규 요소 '테일즈 세이브' 등 상세 정보 공개 - 게임포커스",
+      description:
+        "세가퍼블리싱코리아(대표 사이토 고)가 9월 9일(목) 발매 예정인 '소닉 컬러즈 얼티밋'의 신 요소 '테일즈 세이브'의 상세 정보와 신 위스프 ...",
+      url: 'http://gamefocus.co.kr/detail.php?number=118079',
+      urlToImage:
+        'http://gamefocus.co.kr/wys2/file_attach_thumb/2021/06/03/1622687096_25998_N_7_300x200_100_2_2.jpg',
+      publishedAt: '2021-06-03T02:26:20Z',
+      content:
+        "( ) 9 9() ' ' ' ' ' ' .\r\n' ' 2010 3D ' ' '' .  Nintendo Switch, PlayStation®4, PC(Epic Games Store) .\r\n' ' ' ' .\r\n' ' . .\r\n' ' , , . 1 1 99 .\r\n' ' 2019 'Team Sonic Racing' ' ' .\r\n' ' . , , .\r\n'' '' .… [+102 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: 'Mk.co.kr',
+      },
+      author: 'stock.mk.co.kr',
+      title: '스리랑카서 컨테이너선 침몰에 최악 해양오염 현실화 - 매일경제',
+      description:
+        "스리랑카 앞바다에서 화재가 난 대형 컨테이너선 'MV X-프레스 펄'(MV X-Press Pearl)호가 침몰하기 시작하면서 끔찍한 해양 오염에 대한 우려가 커졌다",
+      url: 'http://vip.mk.co.kr/news/view/21/21/3428571.html',
+      urlToImage: 'https://file.mk.co.kr/news/facebook_mknews.jpg',
+      publishedAt: '2021-06-03T02:09:00Z',
+      content:
+        '\'MV X- \'(MV X-Press Pearl) . AP, AFP MV X- 2() .\r\n(WP) MV X- WP " " .\r\n.\r\n.\r\n\' \' MV X- " " " " .\r\n[https://youtu.be/KT1QGAae1YA]\r\n.\r\n. \r\n.\r\nMV X- 20 18 . \r\n.\r\n186m 1486 278t, 50t, 25t .\r\n12 1 .\r\n, . … [+23 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Khan.co.kr',
+      },
+      author: '//www.facebook.com/sportkh/',
+      title:
+        '“오락실 0.499 타자 기억나? 강백호가 딱 그렇게 치고 있다” - 스포츠경향',
+      description:
+        '47경기를 치렀으니 아직 시즌 초반이지만, 강백호의 스탯은 상식 수준을 넘어선다. 강백호는 2일 경기 포함...',
+      url: 'https://sports.khan.co.kr/sports/sk_index.html?art_id=202106031037003&sec_id=510201',
+      urlToImage:
+        'http://images.khan.co.kr/article/2021/06/03/2021060302000193700031911.jpg',
+      publishedAt: '2021-06-03T01:37:00Z',
+      content:
+        'KT 0.419, 0.495" 0.499 " \r\nKT | \r\n47 , .\r\n 2 0.419, 0.495 . 4 42 . 2 .\r\n 0.495 . KBO 2001 0.503. . 127 .\r\n 2 2015 NC 0.498. 47, 40 KBO 40-40 . 3 1982 MBC 0.497. 72 298 . 2021 KT 0.495 4 .\r\n . , , . ,… [+147 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Sbs.co.kr',
+      },
+      author: '김덕현 기자',
+      title: '신규 확진 681명…인구 대비 백신 1차 접종률 13.1% - SBS 뉴스',
+      description:
+        '코로나19 신규 확진자가 어제 681명 나와서 이틀 연속 6백 명대 후반을 기록했습니다. 신규 확진자 가운데 해외 유입 환자 9명을 제외한 672명이 국내 발생 환자입니다.',
+      url: 'https://news.sbs.co.kr/news/endPage.do?news_id=N1006341883',
+      urlToImage:
+        'https://img.sbs.co.kr/newimg/news/20210603/201557440_1280.jpg',
+      publishedAt: '2021-06-03T01:19:00Z',
+      content: null,
+    },
+    {
+      source: {
+        id: null,
+        name: 'Yna.co.kr',
+      },
+      author: '김정진',
+      title: '한예슬, 버닝썬 연관 의혹에 "수사기관이 밝혀주길" - 연합뉴스',
+      description:
+        '(서울=연합뉴스) 김정진 기자 = 배우 한예슬(40)이 클럽 버닝썬에서의 마약 투약 의혹을 부인하며 "경찰과 검찰에서 (진실을) 밝혀달라"고 했...',
+      url: 'https://www.yna.co.kr/view/AKR20210603049200005',
+      urlToImage:
+        'https://img5.yna.co.kr/photo/yna/YH/2015/01/15/PYH2015011513710001300_P4.jpg',
+      publishedAt: '2021-06-03T01:19:00Z',
+      content:
+        '[ ]\r\n(=) = (40) " () " .\r\n 3 " , . , " .\r\n 2018 .\r\n" " .\r\n " () " " " .\r\n " " " 9 " .\r\n " " .\r\n " . " .\r\n , .\r\nhttps://youtu.be/Nbd2pO4nyn8\r\nstopn@yna.co.kr\r\n&lt;(c) , - &gt;2021/06/03 10:19',
+    },
+  ],
+};
+
+export function getAllArticles() {
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (!data) {
+        const e = new Error('NoArticles');
+        rej(e);
+      }
+      res(data);
+    }, 1500);
+  });
+
+  return promise;
+}
