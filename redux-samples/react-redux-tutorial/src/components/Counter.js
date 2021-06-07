2ba6abc2bcb6fd43162ea78993eedc83f1fd44e4
unknown <wkdus7h@naver.com>
2021-06-07 13:07:40 +0900

    Init project

---
+++ redux-samples/react-redux-tutorial/src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Counter = ({ count, onIncrease, onDecrease }) => {
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
