9dce887943a630d29a3a121badbea921cb3edc53
unknown <wkdus7h@naver.com>
2021-06-07 15:36:51 +0900

    Several ways to link redux-react

--- redux-samples/react-redux-tutorial/src/containers/CounterContainer.js
+++ redux-samples/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 
@@ -7,18 +8,18 @@ const CounterContainer = ({ count, increase, decrease }) => {
   return <Counter count={count} onIncrease={increase} onDecrease={decrease} />;
 };
 
-// 방법 1
-const mapStateToProps = state => ({
-  count: state.counter.count,
-});
-const mapDispatchToProps = dispatch => ({
-  increase: () => dispatch(increase()),
-  decrease: () => dispatch(decrease()),
-});
+// 리덕스 연동 방법 1
+// const mapStateToProps = state => ({
+//   count: state.counter.count,
+// });
+// const mapDispatchToProps = dispatch => ({
+//   increase: () => dispatch(increase()),
+//   decrease: () => dispatch(decrease()),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 
-// 방법2
+// 리덕스 연동 방법2
 // export default connect(
 //   state => ({
 //     count: state.counter.count,
@@ -28,3 +29,29 @@ export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 //     decrease: () => dispatch(decrease()),
 //   }),
 // )(CounterContainer);
+
+// bindActionCreators 쓰기
+// export default connect(
+//   state => ({
+//     count: state.counter.count,
+//   }),
+//   dispatch =>
+//     bindActionCreators(
+//       {
+//         increase,
+//         decrease,
+//       },
+//       dispatch,
+//     ),
+// )(CounterContainer);
+
+// 객체로 액션함수 넣어주기
+export default connect(
+  ({ counter }) => ({
+    count: counter.count,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
