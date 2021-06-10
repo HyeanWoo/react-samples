84c374b6d86f0601c7c72c5bad3a10c285f684e1
unknown <wkdus7h@naver.com>
2021-06-10 14:31:38 +0900

    Convert to hooks

--- redux-samples/react-redux-tutorial/src/containers/CounterContainer.js
+++ redux-samples/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,57 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({ count, increase, decrease }) => {
-  return <Counter count={count} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer = () => {
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
 };
 
-// 리덕스 연동 방법 1
-// const mapStateToProps = state => ({
-//   count: state.counter.count,
-// });
-// const mapDispatchToProps = dispatch => ({
-//   increase: () => dispatch(increase()),
-//   decrease: () => dispatch(decrease()),
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-// 리덕스 연동 방법2
-// export default connect(
-//   state => ({
-//     count: state.counter.count,
-//   }),
-//   dispatch => ({
-//     increase: () => dispatch(increase()),
-//     decrease: () => dispatch(decrease()),
-//   }),
-// )(CounterContainer);
-
-// bindActionCreators 쓰기
-// export default connect(
-//   state => ({
-//     count: state.counter.count,
-//   }),
-//   dispatch =>
-//     bindActionCreators(
-//       {
-//         increase,
-//         decrease,
-//       },
-//       dispatch,
-//     ),
-// )(CounterContainer);
-
-// 객체로 액션함수 넣어주기
-export default connect(
-  ({ counter }) => ({
-    count: counter.count,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
