78277457168703aaae7b3c7003d127aa2bbb8975
unknown <wkdus7h@naver.com>
2021-06-11 19:27:14 +0900

    Refactor create-request-thunk

--- redux-samples/learn-redux-middleware/src/modules/sample.js
+++ redux-samples/learn-redux-middleware/src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 // 액션 타입 선언
 // 액션 타입은 하나의 요청당 세 개를 만들어준다
@@ -13,41 +14,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성
 // thunk 함수 내부에서 시작할 때, 성공했을 때, 실패했을 때 다른 액션은 디스패치해준다
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST }); // 요청 시작한 것을 알림
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해준다
-  }
-};
-
-export const getUsers = () => async dispatch => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태 선업
 const initialState = {
