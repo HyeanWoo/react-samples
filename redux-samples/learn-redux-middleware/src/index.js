76e95e1be0d71b4851b416e34edf353c7aaf96d1
unknown <wkdus7h@naver.com>
2021-06-12 13:29:41 +0900

    Convert counter with redux-saga

--- redux-samples/learn-redux-middleware/src/index.js
+++ redux-samples/learn-redux-middleware/src/index.js
@@ -4,12 +4,18 @@ import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, ReduxThunk, sagaMiddleware),
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
