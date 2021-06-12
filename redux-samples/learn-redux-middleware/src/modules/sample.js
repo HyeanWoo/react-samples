f6c298167175d249ac08fd1968c4a08584354b3e
unknown <wkdus7h@naver.com>
2021-06-12 15:31:24 +0900

    Convert sample with redux-saga

--- redux-samples/learn-redux-middleware/src/modules/sample.js
+++ redux-samples/learn-redux-middleware/src/modules/sample.js
@@ -1,19 +1,61 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
+import { startLoading, finishLoading } from './loading';
 
 // 액션 타입 선언
-// 액션 타입은 하나의 요청당 세 개를 만들어준다
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-// thunk 함수 생성
-// thunk 함수 내부에서 시작할 때, 성공했을 때, 실패했을 때 다른 액션은 디스패치해준다
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST));
+  try {
+    // call을 사용하면 Promise를 반환하는 함수를 호출하고 대기한다
+    // 첫번째 아규먼트는 함수, 나머지는 해당 함수에 넣을 인자
+    const post = yield call(api.getPost, action.payload);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST));
+}
+
+function* getUsersSaga() {
+  yield put(startLoading(GET_USERS));
+  try {
+    const users = yield call(api.getUsers);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // 초기 상태 선업
 const initialState = {
