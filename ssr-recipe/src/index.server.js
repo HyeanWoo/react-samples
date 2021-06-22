54bdced6cc65281855de4d8a5ca137f99bbd64ee
unknown <wkdus7h@naver.com>
2021-06-22 14:48:19 +0900

    Eject & Create index.server.js

---
+++ ssr-recipe/src/index.server.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+const html = ReactDOMServer.renderToString(
+  <div>Hellow Server Side Rendering</div>,
+);
+
+console.log(html);
