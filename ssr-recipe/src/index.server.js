442c1a66de56488e1c8d9de7d89b3fc09848d5f8
unknown <wkdus7h@naver.com>
2021-06-23 18:39:22 +0900

    Offer static files

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -3,6 +3,42 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import { StaticRouter } from 'react-router';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+// asset-manifest.json에서 파일 경로들을 조회
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8'),
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map(key => `<script src="${manifest.files[key]}"</script>`) // 스크립트 태그로 변환
+  .join('');
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+      <meta name="theme-color" content="#000000" />
+      <title>React App</title>
+      <link href="${manifest.files['main.css']}" rel="stylesheet" />
+    </head>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      <script src="${manifest.files['runtime-main.js']}"></script>
+      ${chunks}
+      <script src="${manifest.files['main.js']}"></script>
+    </body>
+    </html>
+  `;
+}
 
 const app = express();
 
@@ -16,9 +52,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx);
-  res.send(root);
+  res.send(createPage(root));
 };
 
+const serve = express.static(path.resolve('./build'), {
+  index: false, // "/" 경로에서 index.html을 보여 주지 않도록 설정
+});
+
+app.use(serve);
 app.use(serverRender);
 
 app.listen(5000, () => {
