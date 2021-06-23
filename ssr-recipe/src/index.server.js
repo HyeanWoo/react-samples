6a5d8863b2c0c3b9c3385bb816d88b5093345a38
unknown <wkdus7h@naver.com>
2021-06-23 18:17:24 +0900

    Write server code

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router';
+import App from './App';
 
-const html = ReactDOMServer.renderToString(
-  <div>Hellow Server Side Rendering!</div>,
-);
+const app = express();
 
-console.log(html);
+// 서버사이드 렌더링을 처리할 핸들러 함수
+// 404를 띄우지않고 처리해준다
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root);
+};
+
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000');
+});
