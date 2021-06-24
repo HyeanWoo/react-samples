216a9f71347ae81239b454b476176eb95c62cec4
unknown <wkdus7h@naver.com>
2021-06-24 17:21:08 +0900

    Use custom hook(usePreloader)

--- ssr-recipe/src/containers/UserContainer.js
+++ ssr-recipe/src/containers/UserContainer.js
@@ -1,20 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import User from '../components/User';
-import { Preloader } from '../lib/PreloadContext';
+import { usePreloader } from '../lib/PreloadContext';
 import { getUser } from '../modules/users';
 
 const UserContainer = ({ id }) => {
   const user = useSelector(state => state.users.user);
   const dispatch = useDispatch();
 
+  usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링 시 API 호출
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) return;
     dispatch(getUser(id));
   }, [dispatch, id, user]);
 
-  if (!user) return <Preloader resolve={() => dispatch(getUser(id))} />;
-
+  if (!user) return null;
   return <User user={user} />;
 };
 
