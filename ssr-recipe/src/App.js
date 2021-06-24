9fe15ca137b9b667f6668fa6f25eb5b7f71aa460
unknown <wkdus7h@naver.com>
2021-06-24 17:07:34 +0900

    Create user component

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import UsersContainer from './containers/UsersContainer';
 import BluePage from './pages/BluePage';
 import RedPage from './pages/RedPage';
+import UsersPage from './pages/UsersPage';
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
-      <Route path="/users" component={UsersContainer} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
