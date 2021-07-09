c1ed81a2005c408aa35e85df198aa2298f17d2b7
Hyeanwoo <wkdus7h@naver.com>
2021-07-09 16:55:58 +0900

    Connect API

--- blog-sample/blog-frontend/src/pages/PostListPage.js
+++ blog-sample/blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PostList from '../components/posts/PostList';
 import HeaderContainer from '../container/common/HeaderContainer';
+import PostListContainer from '../container/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostList />
+      <PostListContainer />
     </>
   );
 };
