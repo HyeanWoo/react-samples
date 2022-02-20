f7b3e65853da8c8e90d53b803cf3b216f0240dfb
HyeanWoo <wkdus7h@naver.com>
2022-02-20 18:07:02 +0900

    Apply react router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,42 +1,19 @@
-import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./router/Home";
+import About from "./router/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    );
-    const json = await response.json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              title={movie.title}
-              coverImg={movie.medium_cover_image}
-              genres={movie.genres}
-              summary={movie.summary}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/movie">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
