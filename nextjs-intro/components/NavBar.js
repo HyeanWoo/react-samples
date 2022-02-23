5b4f5f1a018af03cf2b397c182676a1943d33fdd
HyeanWoo <wkdus7h@naver.com>
2022-02-23 18:12:36 +0900

    Apply CSS module

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -8,10 +9,22 @@ export default function NavBar() {
     <nav>
       <Link href="/">
         {/* Anchor 태그를 사용해 className, style등을 설정할 수 있다. */}
-        <a>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/about" ? styles.active : ""
+          }`}
+        >
+          About
+        </a>
       </Link>
     </nav>
   );
