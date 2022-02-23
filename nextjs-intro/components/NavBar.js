49cc683c465e060b28cf52f07486395671cd12e7
HyeanWoo <wkdus7h@naver.com>
2022-02-23 18:22:01 +0900

    Apply style jsx

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./Navbar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -9,23 +8,25 @@ export default function NavBar() {
     <nav>
       <Link href="/">
         {/* Anchor 태그를 사용해 className, style등을 설정할 수 있다. */}
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/about" ? styles.active : ""
-          }`}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background: beige;
+        }
+
+        a {
+          color: green;
+          text-decoration: none;
+        }
+
+        .active {
+          color: skyblue;
+        }
+      `}</style>
     </nav>
   );
 }
