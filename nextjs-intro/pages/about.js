b40eb71d99437d0da439e2ff7a19c8210fa1d753
HyeanWoo <wkdus7h@naver.com>
2022-02-24 21:16:24 +0900

    Add Helmet component

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,9 @@
+import Helmet from "../components/Helmet";
+
 export default function About() {
   return (
     <div>
+      <Helmet title="About" />
       <h1>About</h1>
     </div>
   );
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,6 +1,9 @@
+import Helmet from "../components/Helmet";
+
 export default function Home() {
   return (
     <div>
+      <Helmet title="Home" />
       <h1>Home</h1>
     </div>
   );
